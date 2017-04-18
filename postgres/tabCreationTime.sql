65a671e1a5cffa07719a0750d978e68802d366b6
Cosimo Simeone <wiwwo@wiwwo.com>
2017-04-18 16:18:09 +0200

    To get table creation date

--- postgres/tabCreationTime.sql
+++ postgres/tabCreationTime.sql
@@ -1,4 +1,5 @@
 SELECT table_name
+      ,table_schema
       ,MIN(MYpg_stat_file.creation) AS creation
       ,MIN(MYpg_stat_file.change) AS change
   FROM information_schema.tables
@@ -6,9 +7,9 @@ SELECT table_name
       ,pg_class
       ,pg_stat_file('./base/' || (pg_ls_dir) || '/' || (relfilenode), TRUE) MYpg_stat_file
  WHERE 1 = 1
-       AND (table_type = 'BASE TABLE' AND table_schema = 'public')
+       AND (table_type = 'BASE TABLE' AND table_schema not in ('pg_catalog','information_schema') )
        AND (pg_ls_dir <> 'pgsql_tmp' AND pg_ls_dir::INT <= (SELECT relfilenode FROM pg_class WHERE relname ILIKE table_name))
        AND (relname ILIKE table_name)
- GROUP BY table_name
- ORDER BY table_name
+ GROUP BY table_name,table_schema
+ ORDER BY table_schema,table_name
 ;
