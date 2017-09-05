41fbdc0d3cea436d7c76025632ed105dc40749a0
Cosimo Simeone <wiwwo@wiwwo.com>
2017-09-05 15:37:47 +0200

    Script to kill sessions

---
+++ postgres/killSess.sql
@@ -0,0 +1,4 @@
+\prompt 'pid to kill? ' pid
+\echo '(trying to) kill pid ':pid
+SELECT pg_terminate_backend(:pid) FROM pg_stat_activity  WHERE  :pid <> pg_backend_pid() AND datname = 'database_name' ;
+
