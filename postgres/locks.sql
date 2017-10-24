f95341222d41efa9a39317e1e80ba53071ecd88d
Cosimo Simeone <wiwwo@wiwwo.com>
2017-10-24 14:38:33 +0200

    Lock checking added

---
+++ postgres/locks.sql
@@ -0,0 +1,5 @@
+SELECT locktype, relation::regclass, mode, transactionid AS tid, virtualtransaction AS vtid, pid, granted
+FROM pg_catalog.pg_locks l
+  LEFT JOIN pg_catalog.pg_database db
+    ON db.oid = l.database
+WHERE NOT pid = pg_backend_pid();
