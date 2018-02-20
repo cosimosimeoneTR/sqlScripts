ffaf0cddfb16a203bdbd5100674b4c468c1a7c2d
Wiwwo <wiwwo+github@wiwwo.com>
2018-02-20 09:26:09 +0100

    No need to show pg_catalog and information_schema

--- postgres/tabSrc.sql
+++ postgres/tabSrc.sql
@@ -1,2 +1,4 @@
 \prompt 'Table name to search? ' tabName
-select * from information_schema.tables where lower(table_name) LIKE lower('%'||:'tabName'||'%');
+select * from information_schema.tables where lower(table_name) LIKE lower('%'||:'tabName'||'%')
+and table_schema not in ('pg_catalog', 'information_schema')
+;
