ec5b8f9a50cca296db969bed7a94e476d24d6f24
Wiwwo <wiwwo+github@wiwwo.com>
2018-02-27 13:46:50 +0100

    Adding client_addr

--- postgres/sess.sql
+++ postgres/sess.sql
@@ -1,1 +1,1 @@
-select pid, datname, usename, application_name, backend_start, xact_start, state, query from pg_stat_activity;
+select pid, datname, usename, application_name, backend_start, client_addr, xact_start, state, query from pg_stat_activity;
