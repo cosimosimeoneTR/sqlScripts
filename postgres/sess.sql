336cea0f99a14d2e61c25c33d572fd2652f3c8db
Wiwwo <wiwwo+github@wiwwo.com>
2018-02-27 14:28:45 +0100

    Show session pid as negative, just to highlight

--- postgres/sess.sql
+++ postgres/sess.sql
@@ -1,1 +1,1 @@
-select pid, datname, usename, application_name, backend_start, client_addr, xact_start, state, query from pg_stat_activity;
+select case when pid = pg_backend_pid() then -pid else pid end pid, datname, usename, application_name, backend_start, client_addr, xact_start, state, query from pg_stat_activity;
