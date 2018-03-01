f303b851163ca7cce9bf90e59b0a195fdd41a01a
Wiwwo <wiwwo+github@wiwwo.com>
2018-03-01 08:23:13 +0100

    Put "now" in a better place

--- postgres/watchSess.sql
+++ postgres/watchSess.sql
@@ -1,2 +1,2 @@
 \prompt 'Sleep in seconds? ' seconds
-select pid, client_addr, to_char(now(),'Dy HH24:MI:SS') now,substr(application_name,1,25) application_name, to_char(xact_start,'Dy HH24:MI:SS') xact_start, state, regexp_replace(substr(query,1,120), E'[\\n\\r]+', '\\n', 'g' )   query from pg_stat_activity where pid <> pg_backend_pid(); \watch :seconds
+select pid, client_addr, substr(application_name,1,25) application_name, to_char(now(),'Dy HH24:MI:SS') now, to_char(xact_start,'Dy HH24:MI:SS') xact_start, state, regexp_replace(substr(query,1,120), E'[\\n\\r]+', '\\n', 'g' )   query from pg_stat_activity where pid <> pg_backend_pid(); \watch :seconds
