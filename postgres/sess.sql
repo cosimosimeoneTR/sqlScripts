fdba0e933d4dcfe956c4b6386624297808549abe
Wiwwo <wiwwo+github@wiwwo.com>
2018-03-02 11:41:34 +0100

    Better ordering

--- postgres/sess.sql
+++ postgres/sess.sql
@@ -1,1 +1,2 @@
-select case when pid = pg_backend_pid() then -pid else pid end pid, datname, usename, application_name, client_addr, to_char(backend_start,'HH24:MI:SS') backend_start, /*xact_start,*/ age(xact_start, backend_start) as age, state, query from pg_stat_activity;
+select case when pid = pg_backend_pid() then -pid else pid end pid, datname, usename, application_name, client_addr, /*to_char(backend_start,'HH24:MI:SS')*/ backend_start, /*xact_start,*/ age(xact_start, backend_start) as age, state, query from pg_stat_activity
+order by backend_start;
