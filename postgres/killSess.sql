6e23ef22f52a29cdcca58fe46c953c48a97c5bd9
Wiwwo <wiwwo+github@wiwwo.com>
2018-03-06 11:10:17 +0100

    LIMITing kill output

--- postgres/killSess.sql
+++ postgres/killSess.sql
@@ -1,12 +1,11 @@
 \prompt 'pid to kill? ' pid
 \echo '(trying to) kill pid ':pid
 SELECT
-    pg_terminate_backend(
-        :pid
-    )
+    pg_terminate_backend(:pid)
 FROM
     pg_stat_activity
 WHERE
     :pid <> pg_backend_pid()
-    AND:'pid' IS NOT NULL;
+    AND:'pid' IS NOT NULL
+LIMIT 1;
 
