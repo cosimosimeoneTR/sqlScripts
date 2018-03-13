a1a5495b1499647be972496c2cf04332883dbe0f
Wiwwo <wiwwo+github@wiwwo.com>
2018-03-13 08:22:15 +0100

    More details

--- postgres/tabSize.sql
+++ postgres/tabSize.sql
@@ -1,9 +1,16 @@
 \prompt 'Table name ? ' tabName
-SELECT
-    pg_size_pretty(
-        SUM( pg_total_relation_size( quote_ident( schemaname )|| '.' || quote_ident( tablename )))::BIGINT
-    ) AS tableSize
-FROM
-    pg_tables
-WHERE
-    LOWER( tablename ) LIKE LOWER( '%' ||:'tabName' || '%' );
+SELECT schemaname, tablename, tableSize FROM (
+  SELECT
+      schemaname,
+      tablename,
+      pg_size_pretty(
+          SUM( pg_total_relation_size( quote_ident( schemaname )|| '.' || quote_ident( tablename )))::BIGINT
+      ) AS tableSize
+      ,SUM( pg_total_relation_size( quote_ident( schemaname )|| '.' || quote_ident( tablename )))::BIGINT as tableSizeRow
+  FROM
+      pg_tables
+  WHERE
+      LOWER( tablename ) LIKE LOWER( '%' ||:'tabName' || '%' )
+  GROUP BY schemaname, tablename
+) X
+ORDER BY tableSizeRow;
